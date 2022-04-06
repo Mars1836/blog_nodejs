3eff3ddb8fbbff4d504cab142037f071fe912a15
HauVu <hauhpll123@gmail.com>
2022-04-06 09:16:10 +0700

    add husky

--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -2,6 +2,6 @@ import express from 'express';
 var router = express.Router();
 import { news } from '../app/controllers/news.controller.js';
 
-                    router.use('/show', news.show);
-    router.use('/', news.index);
+router.use('/show', news.show);
+router.use('/', news.index);
 export default router;
--- src/routes/site.route.js
+++ src/routes/site.route.js
@@ -2,6 +2,6 @@ import express from 'express';
 import { site } from '../app/controllers/site.controller.js';
 
 let siteRouter = express.Router();
-siteRouter.use('/search', site.search);
-siteRouter.use('/', site.index);
+    siteRouter.use('/search', site.search);
+        siteRouter.use('/', site.index);
 export default siteRouter;
